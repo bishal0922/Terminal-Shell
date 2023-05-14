a4d8b733b533da11ea408afbba540ac3b7853d22
Bishal Giri <bishal0922@gmail.com>
2023-05-13 20:43:20 -0500

    add files from local

---
+++ msh.c
@@ -0,0 +1,277 @@
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 Trevor Bakker 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+
+#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
+                                // so we need to define what delimits our tokens.
+                                // In this case  white space
+                                // will separate the tokens on our command line
+
+#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+
+#define MAX_NUM_ARGUMENTS 5     // Mav shell only supports four arguments
+
+#define MAX_HISTORY 15    //Mav shell only supports the last 15 commands
+
+int main()
+{
+  //allocating memory for the command input (string)
+  char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+
+  //creating two history arrays for both the command history and associated pid
+  //TODO->possibly implement linked list
+  char *history[MAX_HISTORY];
+  pid_t pid_history[MAX_HISTORY];
+
+  //historycounter, history index and pid counter and pid history
+  //to keep track of current count and to handle limitations of MAX_HISTORY
+  int his_idx = 0;
+  int pid_idx = 0;
+  int his_ct = 0;
+  int pid_ct = 0;
+  int requested_index;
+
+  //allocating memory for each item in the history array
+  for (int i = 0; i < MAX_HISTORY; i++)
+  {
+    history[i] = (char * ) malloc(MAX_COMMAND_SIZE);
+  }
+
+  //infinite loop
+  while( 1 )
+  {
+    // Print out the msh prompt
+    printf ("msh> ");
+
+    // Read the command from the commandline.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something since fgets returns NULL when there
+    // is no input
+    while(!fgets (command_string, MAX_COMMAND_SIZE, stdin));
+
+    /*
+      HANDLE !n 
+    */
+    if (command_string[0] == '!')
+    {
+      //get the n value from !n 
+      requested_index = atoi(&command_string[1]);
+
+      //if the requested n value is valid (n>0 and n<available history count)
+      if (requested_index >= 0 && requested_index < his_ct)
+      {
+        //change the command from !n to the given value of the history array
+        strcpy(command_string, history[requested_index]);
+      }
+      else
+      {
+        //command is invalid so prompt again
+        printf("Command not found in history.\n");
+        continue;
+      }
+    }
+
+    /* 
+      Parse input 
+    */
+    char *token[MAX_NUM_ARGUMENTS];
+
+    /* Populate the input with null*/
+    for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
+    {
+      token[i] = NULL;
+    }
+
+    int token_count = 0;                                 
+                                                           
+    // Pointer to point to the token
+    // parsed by strsep
+    char *argument_ptr = NULL;                                         
+
+    //string duplicate make a copy of the command        
+    char *working_string  = strdup( command_string );    
+
+    // we are going to move the working_string pointer so
+    // keep track of its original value so we can deallocate
+    // the correct amount at the end
+    char *head_ptr = working_string;
+
+    // Tokenize the input strings with whitespace used as the delimiter
+    while ( ( (argument_ptr = strsep(&working_string, WHITESPACE ) ) != NULL) && 
+              (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup( argument_ptr, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
+        token_count++;
+    }
+
+    //the command isn't null and the user has provided some input/command
+    if (token[0] != NULL)
+    {
+    
+      //adding the provided command to the history array 
+      //while incrementing the current index and the total count 
+      history[his_idx++] = strdup(command_string);
+      his_ct++;
+
+      //use of ternary operators to handle index limit 
+      his_idx = his_idx > MAX_HISTORY - 1 ? 0 : his_idx;
+      his_ct = his_ct > MAX_HISTORY ? MAX_HISTORY: his_ct;
+
+      //handle "quit" and "exit"
+      if (!strcmp(token[0], "quit") || !strcmp(token[0], "exit")) 
+      {
+        exit(0);
+      }
+      //handle cd
+      else if (!strcmp(token[0], "cd"))
+      {//updating the pid array with the parent's pid 
+        pid_history[pid_idx++] = -1;
+        pid_ct++;
+
+        //handling pid index/ array count limitations
+        pid_idx = pid_idx > MAX_HISTORY - 1 ? 0 : pid_idx;
+        pid_ct = pid_ct > MAX_HISTORY ? MAX_HISTORY: pid_ct;
+        //handle chdir fail, invalid directory passed in 
+        if (chdir(token[1]) == -1)
+        {
+          printf("Directory not found.\n");
+        }
+      }
+      //handle history
+      else if (!strcmp(token[0], "history"))
+      {
+        //updating the pid array with the parent's pid 
+        pid_history[pid_idx++] = -1;
+        pid_ct++;
+
+        //handling pid index/ array count limitations
+        pid_idx = pid_idx > MAX_HISTORY - 1 ? 0 : pid_idx;
+        pid_ct = pid_ct > MAX_HISTORY ? MAX_HISTORY: pid_ct;
+        //handle "-p" argument being passed in
+        if (token_count > 2 && !strcmp(token[1], "-p"))
+        {
+          //print the pids with history and their index
+          //iterate through both the pid_history and history array
+          for (int i = 0; i < his_ct; i++)
+          {
+            printf("[%d]: [%d] %s", i, pid_history[i], history[i]);
+          }
+        } 
+        //handle raw history command without arguments
+        else
+        {
+          //iterate through the history array
+          for (int i = 0; i < his_ct; i++)
+          {
+            printf("[%d]: %s", (i), history[i]);
+          }
+        }
+
+      }
+      //we handled quit, exit, cd, and history so the rest is handled by fork()+exec()
+      else 
+      {
+        //create a new process
+        pid_t pid = fork();
+
+        //fork failed 
+        if (pid == -1)
+        {
+          printf("Fork Failed.\n");
+        }
+        //child process
+        else if (pid == 0)
+        {
+          //execute the given process in the child
+          int ret = execvp(token[0], &token[0]);
+
+          //the command was invalid -> exec failed
+          if (ret == -1)
+          {
+            //command doesn't exit
+            printf("%s: Command not found.\n", token[0]);
+            //exit out of the child process
+            exit(0);
+          }
+        }
+        //parent process
+        else 
+        {
+
+          //updating the pid array with the parent's pid 
+          pid_history[pid_idx++] = pid;
+          pid_ct++;
+
+          //handling pid index/ array count limitations
+          pid_idx = pid_idx > MAX_HISTORY - 1 ? 0 : pid_idx;
+          pid_ct = pid_ct > MAX_HISTORY ? MAX_HISTORY: pid_ct;
+          //wait for the child process to end
+          int status;
+          wait(&status);
+
+        }
+      }
+    }
+    
+
+    // Cleanup allocated memory
+    for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
+    {
+      if( token[i] != NULL )
+      {
+        free( token[i] );
+      }
+    }
+
+    free( head_ptr );
+
+  }
+
+  free( command_string );
+
+  //Cleaning up allocated memory for the history array
+  for (int i = 0; i < MAX_HISTORY; i++)
+  {
+    if (history[i] != NULL)
+    {
+      free(history[i]);
+    }
+  }
+
+  return 0;
+  // e2520ca2-76f3-90d6-0242ac120003
+}
